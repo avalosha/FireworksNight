e38a163802d4f7ea2e88febf6c5ebfb43b2c71dd
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-09-30 15:51:59 -0500

    Setup project.

---
+++ Project20/Project20/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Álvaro Ávalos Hernández on 30/09/20.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
