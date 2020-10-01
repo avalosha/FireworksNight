fd70e4a731d3ad184d0fd19fc6cb8bb2355acb94
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-09-30 19:43:37 -0500

    Add timer and SKNode's

--- Project20/Project20/GameScene.swift
+++ Project20/Project20/GameScene.swift
@@ -9,8 +9,118 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var gameTimer: Timer?
+    var fireworks = [SKNode]()
+
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+
+    var score = 0 {
+        didSet {
+            // your code here
+        }
+    }
+    
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+
+        gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
+    }
+    
+    @objc func launchFireworks() {
+        let movementAmount: CGFloat = 1800
+
+            switch Int.random(in: 0...3) {
+            case 0:
+                // fire five, straight up
+                createFirework(xMovement: 0, x: 512, y: bottomEdge)
+                createFirework(xMovement: 0, x: 512 - 200, y: bottomEdge)
+                createFirework(xMovement: 0, x: 512 - 100, y: bottomEdge)
+                createFirework(xMovement: 0, x: 512 + 100, y: bottomEdge)
+                createFirework(xMovement: 0, x: 512 + 200, y: bottomEdge)
+
+            case 1:
+                // fire five, in a fan
+                createFirework(xMovement: 0, x: 512, y: bottomEdge)
+                createFirework(xMovement: -200, x: 512 - 200, y: bottomEdge)
+                createFirework(xMovement: -100, x: 512 - 100, y: bottomEdge)
+                createFirework(xMovement: 100, x: 512 + 100, y: bottomEdge)
+                createFirework(xMovement: 200, x: 512 + 200, y: bottomEdge)
+
+            case 2:
+                // fire five, from the left to the right
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 400)
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 300)
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 200)
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 100)
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge)
+
+            case 3:
+                // fire five, from the right to the left
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 400)
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 300)
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 200)
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 100)
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge)
+
+            default:
+                break
+            }
+    }
+    
+    func createFirework(xMovement: CGFloat, x: Int, y: Int) {
+        // Crea un SKNode para contener los fuegos artificiales
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+
+        // Crea un nodo de sprite con ajuste para modificar su color
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.colorBlendFactor = 1
+        firework.name = "firework"
+        node.addChild(firework)
+
+        // Colorea el nodo de sprite anterior
+        switch Int.random(in: 0...2) {
+        case 0:
+            firework.color = .cyan
+
+        case 1:
+            firework.color = .green
+
+        case 2:
+            firework.color = .red
+
+        default:
+            break
+        }
+
+        // Crea un UIBezierPath para representar el movimientos de los fuegos artificiales
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: xMovement, y: 1000))
+
+        // Se indica al nodo que siga el movimiento establecido
+        //El primer parametro decide si las coordenadas de la ruta son absolutas o relativas a la posición actual del nodo
+        //asOffset como verdadero, significa que las coordenadas en su ruta se ajustan para tener en cuenta la posición del nodo
+        //orientToPath cuando se establece en verdadero, el nodo rotará automáticamente a medida que se mueve en la ruta para que siempre esté mirando hacia abajo.
+        //especificar una velocidad para ajustar qué tan rápido se mueve a lo largo del camino.
+        let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: 200)
+        node.run(move)
+
+        // Crea las partículas atras del cohete
+        if let emitter = SKEmitterNode(fileNamed: "fuse") {
+            emitter.position = CGPoint(x: 0, y: -22)
+            node.addChild(emitter)
+        }
+
+        // Añade los fuegos artificiales
+        fireworks.append(node)
+        addChild(node)
     }
     
 }
